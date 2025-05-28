00a8a76e055de054b93151a3f637bfcd81777444
brentjanssen <145325898+brentjanssen@users.noreply.github.com>
2025-05-28 16:45:15 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: "at6bum",
@@ -6,5 +6,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false,
+    // No custom specPattern - let it use defaults
   },
-});
+})
